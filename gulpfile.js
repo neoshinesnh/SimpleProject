1d0e0458f073d8506bebad70d29bcc222af1564a
Elton Martins <eltonseiji@gmail.com>
2016-11-14 17:54:34 -0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 // Gulp dependencies go here
 
 gulp.task('default', function(){
-	// Gulp tasks go here
+	gulp.src("es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
+
+	// browser source
+	gulp.src("public/es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
 });
+
