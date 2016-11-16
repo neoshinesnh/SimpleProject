bb2a04333cbee705aa1095f40edc06719195e3ff
Elton Martins <eltonseiji@gmail.com>
2016-11-16 11:38:24 -0200

    Configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-// Gulp dependencies go here
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function(){
+	// Run ESLint
+	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
+
+	// Node source
 	gulp.src("es6/**/*.js")
 		.pipe(babel())
 		.pipe(gulp.dest("dist"));
-
-	// browser source
+		
+	// Browser source
 	gulp.src("public/es6/**/*.js")
 		.pipe(babel())
 		.pipe(gulp.dest("public/dist"));
